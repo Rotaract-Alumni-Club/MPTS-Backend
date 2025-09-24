b5d2f68904a0465c0eabb9cd862e54a8beb5e26e
ThulaniThehara <athukoralaatt.23@uom.lk>
2025-09-24 17:44:13 +0530

    Adding Member Count to Committee

--- Models/Committee.model.js
+++ Models/Committee.model.js
@@ -4,6 +4,7 @@ const committeeSchema = new mongoose.Schema({
     CName : { type: String, required: true, unique: true },
     Description : { type: String, required: true },
     Coordinator : { type: String, required: true },
+    MemberCount: {type:Number, required:true}
 }, { timestamps: true });
 
 module.exports = committeeSchema;
