27bd35d69166c9de5b44b5b66ab126e51380b405
ThulaniThehara <athukoralaatt.23@uom.lk>
2025-09-26 20:05:44 +0530

    hri

--- Routes/Committee.route.js
+++ Routes/Committee.route.js
@@ -3,11 +3,11 @@ const committeeController = require("../Controllers/Committee.controller");
 
 const router = express.Router();
 
-router.post('/committee',committeeController.addCommittee);
-router.get('/committee',committeeController.getCommittees);
-router.get('/committee/:id',committeeController.getCommitteesByID);
-router.get('/committee/N/:name',committeeController.getCommitteesByName);
-router.get('/committee/Co/:Coname',committeeController.getCommitteesByCoordinator);
-router.get('/committee/Mem/:Count',committeeController.getCommitteesByMemCount);
+router.post('/add',committeeController.addCommittee);
+router.get('/get',committeeController.getCommittees);
+router.get('/id/:id',committeeController.getCommitteesByID);
+router.get('/name/:name',committeeController.getCommitteesByName);
+router.get('/coordinator/:Coname',committeeController.getCommitteesByCoordinator);
+router.get('/mem/:Count',committeeController.getCommitteesByMemCount);
 
 module.exports = router;
