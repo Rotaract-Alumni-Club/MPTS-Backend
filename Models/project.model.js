dfe3f878546de825708562ee9ca947b802b5f270
Sehara2002 <shehara1010@gmail.com>
2025-09-27 16:37:19 +0530

    Modified Project Folder

--- Models/project.model.js
+++ Models/project.model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection.useDb("MPTS");
 
-
 const projectSchema = new mongoose.Schema({
     PName : { type: String, required: true, unique: true },
     StartDate : { type: Date, required: true },
